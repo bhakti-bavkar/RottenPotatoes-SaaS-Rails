8d7dfba07c5bc16cfe9fb4da8e6d669528314419
bhaktibavkar <bhakti.t88@gmail.com>
2017-07-13 18:55:43 +0000

    Changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rottenpotatoes::Application.routes.draw do
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies') # root 'movies#index'
+  root 'movies#index'
   resources :movies do
     member do
       get 'search' 
