de5012fbf45827493c98379b50d585ea4ea4251d
bhaktibavkar <bhakti.t88@gmail.com>
2017-07-18 04:46:40 +0000

    Added delete confirmation and released date params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   
   before_action :get_settings
   after_action :set_settings
-  
+
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
@@ -40,12 +40,17 @@ class MoviesController < ApplicationController
 
   def new
     # default: render 'new' template
+    @movie = Movie.new
   end
 
   def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -54,9 +59,12 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
+    if @movie.update_attributes(movie_params)
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      redirect_to movie_path(@movie)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,29 @@
 class Movie < ActiveRecord::Base
+
+  #before_save :capitalize_title
+  #def capitalize_title
+  #  self.title = self.title.split(/\s+/).map(&:downcase).
+  #    map(&:capitalize).join(' ')
+  #end
+  
   def self.all_ratings
-        ['G','PG','PG-13','R','NC-17']
+    ['G','PG','PG-13','R','NC-17']
+  end
+  
+  @@grandfathered_date = Date.parse('1 Nov 1968')
+
+  validates :title, :presence => true
+  validates :release_date, :presence => true
+  validate :released_1930_or_later # uses custom validator below
+  validates :rating, :inclusion => {:in => Movie.all_ratings},
+    :unless => :grandfathered?
+  
+  def released_1930_or_later
+    errors.add(:release_date, 'must be 1930 or later') if
+      release_date && release_date < Date.parse('1 Jan 1930')
+  end
+  def grandfathered?
+    release_date && release_date < @@grandfathered_date
   end
   
   def self.search_movies_by_director(director)
